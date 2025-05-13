d75f6b7086c1ec43bda3095c57f93eeecefb0f81
YourGitHubUsername <abhishekbiradar2856@gmail.com>
2025-05-13 17:12:51 +0530

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavBar from "./NavBar";
 
 function App() {
@@ -5,6 +6,15 @@ function App() {
 
   return (
     <>
+    <BrowserRouter basename="/">
+
+<Routes>
+  <Route path="/" element={<div>Base page</div>}></Route>
+  <Route path="/login" element={<div>Login page</div>}></Route>
+  <Route path="/test" element={<div>Test page</div>}></Route>
+</Routes>
+
+    </BrowserRouter>
      <NavBar/>
       <h1 className="text-4xl font-extrabold">Hello World</h1>
     </>
