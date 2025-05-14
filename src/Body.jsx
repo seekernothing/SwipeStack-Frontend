d8a92bed938eaf12a1fa74e868e602ee3b5072df
YourGitHubUsername <abhishekbiradar2856@gmail.com>
2025-05-14 23:09:49 +0530

    minor changes

--- src/Body.jsx
+++ src/Body.jsx
@@ -6,6 +6,7 @@ import Footer from "./Footer";
 const Body = () => {
   return (
     <div className="min-h-screen flex flex-col">
+    
       <NavBar />
       {/* // Any children routes of the body will render in the outlet */}
       <Outlet />
