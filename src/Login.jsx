480998e8dcbad8eb533ac3c5713be57e816b61e7
YourGitHubUsername <abhishekbiradar2856@gmail.com>
2025-05-16 17:58:11 +0530

    added axios and login button handler

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,6 +1,24 @@
+import { useState } from "react";
+import axios from "axios";
+
 const Login = () => {
+  const [emailId, setEmailId] = useState("rohitsharma264@gmail.com");
+  const [password, setPassword] = useState("Rohit@123");
+
+  const handelLogin = async () => {
+    try {
+      await axios.post("http://localhost:8000/login", {
+       email: emailId,
+        password,
+      },{withCredentials:true});
+    } catch (error) {
+      console.error("Login error:", error.message);
+      // Optionally, set an error state here to display a message to the user
+    }
+  };
+
   return (
-    <div className="flex justify-center items-center p-15">
+    <div className="flex justify-center items-center p-10">
       <div className="card card-border bg-base-300 w-90">
         <div className="card-body">
           <h2 className="card-title justify-center text-4xl "> Welcome Back</h2>
@@ -9,12 +27,24 @@ const Login = () => {
           <div>
             <fieldset className="fieldset ">
               <legend className="fieldset-legend underline ">Email ID</legend>
-              <input type="text" className="input" placeholder="" />
+              <input
+                type="text"
+                className="input"
+                placeholder=""
+                value={emailId}
+                onChange={(e) => setEmailId(e.target.value)}
+              />
             </fieldset>
 
             <fieldset className="fieldset ">
               <legend className="fieldset-legend underline ">Password</legend>
-              <input type="text" className="input" placeholder="" />
+              <input
+                type="text"
+                className="input"
+                placeholder=""
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </fieldset>
           </div>
 
@@ -31,7 +61,9 @@ const Login = () => {
           </div>
 
           <div className="card-actions justify-center mt-3">
-            <button className="btn btn-primary">Login</button>
+            <button onClick={handelLogin} className="btn btn-primary">
+              Login
+            </button>
           </div>
         </div>
       </div>
