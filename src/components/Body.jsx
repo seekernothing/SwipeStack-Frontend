64bc75c60034e50302f1e97b052bab89ccdb5234
YourGitHubUsername <abhishekbiradar2856@gmail.com>
2025-05-17 18:19:55 +0530

    security check for login

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,18 +1,42 @@
-import { Outlet } from "react-router-dom"
-import NavBar from "./NavBar"
+import { Navigate, Outlet, useNavigate } from "react-router-dom";
+import NavBar from "./NavBar";
 import Footer from "./Footer";
-
+import { BASE_URL } from "../utils/Constants";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useEffect } from "react";
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const Navigate = useNavigate();
+  const userData = useSelector((store) => store.user);
+  const fetchUser = async () => {
+    if (userData) return;
+    try {
+      const res = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+      dispatch(addUser(res.data));
+    } catch (error) {
+      if (error.status === 401) {
+        Navigate("/login");
+      }
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
   return (
     <div className="min-h-screen flex flex-col">
-    
       <NavBar />
       {/* // Any children routes of the body will render in the outlet */}
       <Outlet />
       <Footer />
     </div>
   );
-}
+};
 
-export default Body
+export default Body;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@
 
 const Footer = () => {
   return (
-    <footer className="footer footer-horizontal footer-center bg-[rgb(8,9,10)] text-base-content rounded p-10">
+    <footer className="footer footer-horizontal footer-center bg-[rgb(8,9,10)] text-base-content rounded p-10 ">
       <nav className="grid grid-flow-col gap-4">
         <a className="link link-hover">About us</a>
         <a className="link link-hover">Contact</a>
